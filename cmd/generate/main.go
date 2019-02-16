5230f20f1f6664a1ffe6e00bb142eada4404e788
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-02-16 19:22:56 +0300

    out conditions for google pizza

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -13,7 +13,10 @@ import (
 )
 
 func main() {
-	size := flag.String("s", "", "usage size")
+	size := flag.String("size", "", "size of pizza")
+	minCount := flag.Int("min", 1, "min count each ingredient")
+	maxCount := flag.Int("max", 6, "max number of ceil")
+	file := flag.String("file", "", "path to file")
 	flag.Parse()
 
 	sizeRegexp := regexp.MustCompile(`^\d+\s?[xX]\s?\d+$`)
@@ -41,7 +44,25 @@ func main() {
 	height, _ := strconv.ParseInt(result[0], 10, 32)
 	width, _ := strconv.ParseInt(result[1], 10, 32)
 
-	fmt.Printf("size: %dx%d\r\n", height, width)
+	fmt.Printf("Size: %dx%d\r\n", height, width)
+
+	fmt.Println("Conditions:")
+	fmt.Printf("Min count each ingredient: %d\r\n", *minCount)
+	fmt.Printf("Max size: %d\r\n", *maxCount)
+
+	for *file == "" {
+		fmt.Println("Input output file for:")
+		s := bufio.NewScanner(os.Stdin)
+		for s.Scan() {
+			line := s.Text()
+			if len(line) > 0 {
+				file = &line
+				break
+			}
+		}
+	}
+
+	fmt.Printf("File: %s\r\n", *file)
 
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
