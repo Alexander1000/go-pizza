e157dbc896d993b885ca4c418719ccf60b976f6b
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-02-24 14:15:58 +0300

    fixes

--- src/go-pizza/slicer/loader.go
+++ src/go-pizza/slicer/loader.go
@@ -26,65 +26,48 @@ func Load(file string) (*Slicer, error) {
 		return nil, errors.New("empty file")
 	}
 
-	heightSize := 0
-	for _, b := range buffer {
-		if b == byte(0x20) {
-			break
-		}
-		if b >= byte('0') && b <= byte('9') {
-			heightSize++
-		} else {
-			err = errors.New("invalid character")
-			break
-		}
-	}
-
-	if heightSize == 0 {
-		return nil, errors.New("empty height")
-	}
+	height, err := scanDigit(buffer)
 	if err != nil {
 		return nil, err
-	}
-
-	height, err := strconv.ParseInt(string(buffer[0:heightSize]), 10, 64)
-	if err != nil {
-		return nil, err
-	}
-	if height < 0 {
-		return nil, errors.New("invalid height size")
 	}
 
 	log.Printf("Height: %d", height)
 
-	widthSize := 0
-	for _, b := range buffer[heightSize+1:] {
-		if b == byte(0x20) {
-			break
-		}
-		if b >= byte('0') && b <= byte('9') {
-			widthSize++
-		} else {
-			err = errors.New("invalid character")
-			break
-		}
-	}
+	heightSize := len(strconv.Itoa(int(height)))
 
-	if widthSize == 0 {
-		return nil, errors.New("empty width")
-	}
+	width, err := scanDigit(buffer[heightSize+1:])
 	if err != nil {
 		return nil, err
-	}
-
-	width, err := strconv.ParseInt(string(buffer[heightSize+1:heightSize + 1 + widthSize]), 10, 64)
-	if err != nil {
-		return nil, err
-	}
-	if width < 0 {
-		return nil, errors.New("invalid width size")
 	}
 
 	log.Printf("Width: %d", width)
 
 	return nil, nil
+}
+
+func scanDigit(buffer []byte) (int64, error) {
+	size := 0
+	for _, b := range buffer {
+		if b == byte(0x20) {
+			break
+		}
+		if b >= byte('0') && b <= byte('9') {
+			size++
+		} else {
+			return 0, errors.New("invalid character")
+		}
+	}
+
+	if size == 0 {
+		return 0, errors.New("digits not found")
+	}
+
+	digit, err := strconv.ParseInt(string(buffer[:size]), 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	if digit < 0 {
+		return 0, errors.New("invalid digit value")
+	}
+	return digit, nil
 }
