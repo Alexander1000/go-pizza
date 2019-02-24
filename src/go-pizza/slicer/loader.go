57eab1dc545ef7d2f23cf9410b3fae647039f3b3
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-02-24 14:21:44 +0300

    scan input conditions

--- src/go-pizza/slicer/loader.go
+++ src/go-pizza/slicer/loader.go
@@ -42,13 +42,32 @@ func Load(file string) (*Slicer, error) {
 
 	log.Printf("Width: %d", width)
 
+	widthSize := len(strconv.Itoa(int(width)))
+
+	minIngredienCount, err := scanDigit(buffer[heightSize+1+widthSize+1:])
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Min ingredient count: %d", minIngredienCount)
+
+	minIngredienCountSize := len(strconv.Itoa(int(minIngredienCount)))
+
+	maxSizeSlice, err := scanDigit(buffer[heightSize+1+widthSize+1+minIngredienCountSize+1:])
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("Max slice size: %d", maxSizeSlice)
+
+
 	return nil, nil
 }
 
 func scanDigit(buffer []byte) (int64, error) {
 	size := 0
 	for _, b := range buffer {
-		if b == byte(0x20) {
+		if b == byte(0x20) || b == byte(0x0D) {
 			break
 		}
 		if b >= byte('0') && b <= byte('9') {
