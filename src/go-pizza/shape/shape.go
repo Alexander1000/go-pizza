378c65689b2512723a7a637e8e72d8f1b1076183
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-03-02 17:18:40 +0300

    fixes

---
+++ src/go-pizza/shape/shape.go
@@ -0,0 +1,13 @@
+package shape
+
+type Shape struct {
+	Height int
+	Width int
+}
+
+// Generate - возвращает все возможные варианты фигур для которых будет соблюдено
+// min - минимальная площадь, max - максимальная площадь
+func Generate(min int, max int) []Shape {
+	result := make([]Shape, 0)
+	return result
+}
