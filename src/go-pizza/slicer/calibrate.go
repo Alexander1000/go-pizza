10547a40ede4c1b48a963d070e3eacd1aded1478
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-03-04 13:41:10 +0300

    update

--- src/go-pizza/slicer/calibrate.go
+++ src/go-pizza/slicer/calibrate.go
@@ -1,5 +1,18 @@
 package slicer
 
 func (s *Slicer) calibrate() {
+	blackPoints := make([]BlackPoint, 0)
 
+	for i := int64(0); i < s.height; i++ {
+		for j := int64(0); j < s.width; j++ {
+			if !s.filled[i * s.width + j] {
+				blackPoints = append(blackPoints, BlackPoint{X: j, Y: i})
+			}
+		}
+	}
+}
+
+type BlackPoint struct {
+	X int64
+	Y int64
 }
