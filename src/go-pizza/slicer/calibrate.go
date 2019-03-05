5dd761ebafb01741bf82cf78b0601fd8299a2f26
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-03-05 14:11:01 +0300

    update

--- src/go-pizza/slicer/calibrate.go
+++ src/go-pizza/slicer/calibrate.go
@@ -44,11 +44,11 @@ func (s *Slicer) calibrate() {
 
 	for _, blackPoint := range blackPointList {
 		// try optimize/ defragmentation
-		startX := math.Max(float64(blackPoint.X) - float64(s.maxSlice) * 1.2, 0)
-		startY := math.Max(float64(blackPoint.Y) - float64(s.maxSlice) * 1.2, 0)
+		startX := math.Max(float64(blackPoint.X) - float64(s.maxSlice) * 1.5, 0)
+		startY := math.Max(float64(blackPoint.Y) - float64(s.maxSlice) * 1.5, 0)
 
-		stopX := math.Min(float64(blackPoint.X) + float64(s.maxSlice) * 1.2, float64(s.width))
-		stopY := math.Min(float64(blackPoint.Y) + float64(s.maxSlice) * 1.2, float64(s.height))
+		stopX := math.Min(float64(blackPoint.X) + float64(s.maxSlice) * 1.5, float64(s.width))
+		stopY := math.Min(float64(blackPoint.Y) + float64(s.maxSlice) * 1.5, float64(s.height))
 
 		subSlice := s.importToSubSlice(int64(startX), int64(startY), int64(stopX), int64(stopY))
 		if subSlice == nil {
